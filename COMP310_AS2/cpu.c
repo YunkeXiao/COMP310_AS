aeb312fc2ef954625f754135fe940f2bea0e445c
Yunke Xiao <yunke0123@hotmail.com>
2020-02-28 17:11:51 -0500

    add CPU initializer

--- COMP310_AS2/cpu.c
+++ COMP310_AS2/cpu.c
@@ -1,6 +1,9 @@
+#include <stdlib.h>
 #include "cpu.h"
 #include "CPU.h"
 
-struct *CPU createCPU(int quanta){
-
+struct CPU* createCPU(int quanta){
+    struct CPU *cpu = malloc(sizeof(struct CPU));
+    cpu->quanta = quanta;
+    return cpu;
 }
